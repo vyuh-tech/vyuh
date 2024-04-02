92572eb1749905a576830ad62194679e9fba1412
pavanpodila <pavan@vyuh.tech>
2024-04-02 08:57:44 +0530

    doc: added documentation for all the classes refactor: simplified the folder structure with lesser nesting

--- packages/sanity/flutter_sanity_portable_text/lib/ui/index.dart
+++
@@ -1,3 +0,0 @@
-export 'portable_text_block.dart';
-export 'portable_text_config.dart';
-export 'portable_text_widget.dart';
--- packages/sanity/flutter_sanity_portable_text/lib/ui/portable_text_widget.dart
+++ packages/sanity/flutter_sanity_portable_text/lib/ui/portable_text_widget.dart
@@ -2,11 +2,20 @@ import 'package:flutter/material.dart';
 
 import '../flutter_sanity_portable_text.dart';
 
+/// A widget that renders a list of PortableBlockItems. This widget is the main entry point for
+/// rendering Portable Text content. It is responsible for rendering the entire content of a Portable
+/// Text document, including all blocks and marks. It relies on the [PortableTextConfig] to determine
+/// the visual representation of each block.
 class PortableText extends StatelessWidget {
+  /// The list of block items to render.
   final List<PortableBlockItem> blocks;
 
+  /// Whether the to treat this as a primary scroll view. This is useful when you want to have a
+  /// single scroll view for the entire content. It is false by default.
   final bool usePrimaryScroller;
 
+  /// Whether to shrink-wrap the list view. This is useful when you want the list view to take up
+  /// only the space it needs. It is true by default.
   final bool shrinkwrap;
 
   const PortableText({
