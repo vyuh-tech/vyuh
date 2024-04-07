4b8aad2ff743511e9496b91015e8b1b850a4965c
pavanpodila <pavan@vyuh.tech>
2024-04-07 19:52:07 +0530

    feat: added single item layout

---
+++ packages/system/vyuh_feature_system/lib/content/route/single_item_layout.g.dart
@@ -0,0 +1,12 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'single_item_layout.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+SingleItemLayout _$SingleItemLayoutFromJson(Map<String, dynamic> json) =>
+    SingleItemLayout(
+      useSafeArea: json['useSafeArea'] as bool? ?? false,
+    );
