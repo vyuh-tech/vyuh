e411760215a954934d834bb19824fb1b1319924d
pavanpodila <pavan@vyuh.tech>
2024-04-11 16:44:34 +0530

    feat: added a mason brick to generate a vyuh project with sanity and flutter

---
+++ apps/studio/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'ox69wzz5',
+    dataset: 'production'
+  }
+})
--- bricks/vyuh_init/hooks/post_gen.dart
+++ bricks/vyuh_init/hooks/post_gen.dart
@@ -6,18 +6,78 @@ import 'package:mason/mason.dart';
 Future<void> run(HookContext context) async {
   final String name = context.vars['name'];
   final String description = context.vars['description'];
+  final String cms = context.vars['cms'];
+  final appName = name.snakeCase;
 
-  await Process.run(
-    'flutter',
-    [
-      'create',
-      '${name.snakeCase}',
-      '--template=app',
-      '--platforms=ios,android,web',
-      '--description=$description',
-    ],
-    workingDirectory: 'apps',
+  await _trackOperation(
+    context,
+    startMessage: 'Setting up the Flutter project @ apps/$appName',
+    endMessage: 'Flutter project ready @ apps/$appName',
+    operation: () async {
+      await Process.run(
+        'flutter',
+        [
+          'create',
+          '${name.snakeCase}',
+          '--template=app',
+          '--platforms=ios,android,web',
+          '--description=$description',
+        ],
+        workingDirectory: 'apps',
+      );
+    },
   );
 
-  await Process.run('melos', ['bootstrap']);
+  await _trackOperation(
+    context,
+    startMessage: 'Running `melos bootstrap` on apps/$appName',
+    endMessage: 'Melos bootstrap complete for apps/$appName',
+    operation: () async {
+      await Process.run('melos', ['bootstrap']);
+    },
+  );
+
+  if (cms == 'sanity') {
+    final studioName = '${name.paramCase}-studio';
+
+    await _trackOperation(
+      context,
+      startMessage: 'Setting up the Sanity project @ apps/$studioName',
+      endMessage: 'Sanity project ready @ apps/$studioName',
+      operation: () async {
+        await Process.run(
+          'pnpm',
+          [
+            'create',
+            'sanity@latest',
+            '--yes',
+            '--dataset',
+            'production',
+            '--template',
+            'clean',
+            '--visibility',
+            'public',
+            '--create-project',
+            '$studioName',
+            '--output-path',
+            '$studioName',
+          ],
+          workingDirectory: 'apps',
+        );
+      },
+    );
+  }
+}
+
+Future<void> _trackOperation(
+  HookContext context, {
+  required String startMessage,
+  required String endMessage,
+  required Future<void> Function() operation,
+}) async {
+  final progress = context.logger.progress(startMessage);
+
+  await operation();
+
+  progress.complete(endMessage);
 }
