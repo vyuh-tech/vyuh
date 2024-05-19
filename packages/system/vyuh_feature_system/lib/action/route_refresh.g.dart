345e29c1e00b9a0230fbb44cb6a41712866ffcf0
pavanpodila <pavan@vyuh.tech>
2024-05-19 12:52:14 +0530

    feat: added a route refresh action

---
+++ packages/system/vyuh_feature_system/lib/action/route_refresh.g.dart
@@ -0,0 +1,12 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'route_refresh.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+RouteRefreshAction _$RouteRefreshActionFromJson(Map<String, dynamic> json) =>
+    RouteRefreshAction(
+      title: json['title'] as String?,
+    );
