0ce9e7d68370f17d814565723d233ac80511eeba
pavanpodila <pavan@vyuh.tech>
2024-06-06 19:21:46 +0530

    feat: added the alert action

---
+++ packages/system/vyuh_feature_system/lib/action/alert.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'alert.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+ShowAlertAction _$ShowAlertActionFromJson(Map<String, dynamic> json) =>
+    ShowAlertAction(
+      title: json['title'] as String?,
+      message: json['message'] as String? ?? '',
+      actions: (json['actions'] as List<dynamic>?)
+              ?.map((e) => UserAction.fromJson(e as Map<String, dynamic>))
+              .toList() ??
+          const [],
+      barrierDismissible: json['barrierDismissible'] as bool? ?? true,
+    );
+
+UserAction _$UserActionFromJson(Map<String, dynamic> json) => UserAction(
+      action: json['action'] == null
+          ? null
+          : Action.fromJson(json['action'] as Map<String, dynamic>),
+      title: json['title'] as String? ?? 'Title',
+    );
