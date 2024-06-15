c550b1d83d83204277442b762ae96f14f90354c7
pavanpodila <pavan@vyuh.tech>
2024-06-15 16:19:23 +0530

    feat: adding ability to resolve a CMS path from a local path. It's currently one-directional...local->CMS

--- packages/system/vyuh_core/lib/runtime/cms_route.dart
+++ packages/system/vyuh_core/lib/runtime/cms_route.dart
@@ -1,16 +1,47 @@
-import 'package:go_router/go_router.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart' hide RouteBase;
+import 'package:vyuh_core/vyuh_core.dart';
+
+typedef CMSPathResolver = String Function(String path);
+
+String identityPathResolver(String path) => path;
 
 /// A route purely meant to identify CMS pages. Use it to distinguish between the way
 /// local routes and CMS routes are handled.
 final class CMSRoute extends GoRoute {
+  final CMSPathResolver cmsPathResolver;
+
   CMSRoute({
     required super.path,
+    this.cmsPathResolver = identityPathResolver,
     super.builder,
-    super.pageBuilder,
+    GoRouterPageBuilder? pageBuilder,
     super.parentNavigatorKey,
     super.name,
     super.onExit,
     super.redirect,
     super.routes,
-  });
+  }) : super(
+          pageBuilder: pageBuilder == null && redirect == null
+              ? defaultRoutePageBuilder
+              : pageBuilder,
+        );
+}
+
+Page<dynamic> defaultRoutePageBuilder(
+    BuildContext context, GoRouterState state) {
+  final route = state.extra as RouteBase?;
+
+  if (route == null) {
+    final path = state.topRoute is CMSRoute
+        ? (state.topRoute as CMSRoute).cmsPathResolver(state.matchedLocation)
+        : state.matchedLocation;
+
+    return MaterialPage(
+      child: vyuh.content.buildRoute(context, url: Uri.parse(path)),
+      name: state.matchedLocation,
+    );
+  }
+
+  return route.createPage(context);
 }
