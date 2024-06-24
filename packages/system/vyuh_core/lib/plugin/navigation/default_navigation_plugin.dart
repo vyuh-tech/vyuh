38aebc520f7315a245b79c3512fef696a415f52e
pavanpodila <pavan@vyuh.tech>
2024-06-24 17:42:06 +0530

    feat: pulling some go router configuration into the default navigation plugin

--- packages/system/vyuh_core/lib/plugin/navigation/default_navigation_plugin.dart
+++ packages/system/vyuh_core/lib/plugin/navigation/default_navigation_plugin.dart
@@ -1,71 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 import 'package:go_router/go_router.dart' as g;
 import 'package:go_router/go_router.dart';
 import 'package:vyuh_core/runtime/platform/default_platform.dart';
-import 'package:vyuh_core/runtime/platform/fallback_route_page_builder.dart';
 import 'package:vyuh_core/vyuh_core.dart';
-
-abstract class NavigationPlugin extends Plugin {
-  GoRouter get instance;
-
-  static final fallbackRoute = GoRoute(
-    path: '/:path(.*)',
-    pageBuilder: fallbackRoutePageBuilder,
-  );
-
-  NavigationPlugin({required super.name, required super.title})
-      : super(pluginType: PluginType.navigation);
-
-  void initRouter({
-    String? initialLocation,
-    required List<g.RouteBase> routes,
-    required GlobalKey<NavigatorState> rootNavigatorKey,
-  });
-
-  void replaceRoutes(List<g.RouteBase> routes);
-
-  void appendRoutes(List<g.RouteBase> routes);
-
-  void pop<T extends Object?>([T? result]);
-
-  void go(String location, {Object? extra});
-
-  void goNamed(
-    String name, {
-    Map<String, String> pathParameters = const <String, String>{},
-    Map<String, dynamic> queryParameters = const <String, dynamic>{},
-    Object? extra,
-  });
-
-  Future<T?> push<T extends Object?>(String location, {Object? extra});
-
-  Future<T?> pushNamed<T extends Object?>(
-    String name, {
-    Map<String, String> pathParameters = const <String, String>{},
-    Map<String, dynamic> queryParameters = const <String, dynamic>{},
-    Object? extra,
-  });
-
-  Future<T?> pushReplacement<T extends Object?>(String location,
-      {Object? extra});
-
-  Future<T?> pushReplacementNamed<T extends Object?>(
-    String name, {
-    Map<String, String> pathParameters = const <String, String>{},
-    Map<String, dynamic> queryParameters = const <String, dynamic>{},
-    Object? extra,
-  });
-
-  Future<T?> replace<T extends Object?>(String location, {Object? extra});
-
-  Future<T?> replaceNamed<T extends Object?>(
-    String name, {
-    Map<String, String> pathParameters = const <String, String>{},
-    Map<String, dynamic> queryParameters = const <String, dynamic>{},
-    Object? extra,
-  });
-}
 
 final class DefaultNavigationPlugin extends NavigationPlugin {
   GoRouter _router = GoRouter(routes: []);
@@ -80,6 +19,18 @@ final class DefaultNavigationPlugin extends NavigationPlugin {
 
   @override
   GoRouter get instance => _router;
+
+  static void enableURLReflectsImperativeAPIs() {
+    GoRouter.optionURLReflectsImperativeAPIs = true;
+  }
+
+  static void usePathStrategy() {
+    usePathUrlStrategy();
+  }
+
+  static void useHashStrategy() {
+    useHashStrategy();
+  }
 
   @override
   void initRouter(
