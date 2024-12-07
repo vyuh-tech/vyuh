f42debcc0b9aea85e26928db266066078e4911ef
pavanpodila <pavan@vyuh.tech>
2024-12-07 16:44:39 +0530

    fix: adding a test behavior like debug

---
+++ packages/system/vyuh_core/lib/plugin/content/debug_modifier.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:vyuh_core/plugin/content/content_item.dart';
+
+final class DebugModifier extends ContentModifierConfiguration {
+  DebugModifier() : super(schemaType: 'vyuh.content.modifier.debug');
+
+  @override
+  Widget build(BuildContext context, Widget child, ContentItem content) {
+    final theme = Theme.of(context);
+
+    return Stack(
+      children: [
+        child,
+        Positioned(
+          top: 2,
+          right: 2,
+          child: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 2),
+            color: theme.colorScheme.inverseSurface.withOpacity(0.5),
+            child: Text(
+              content.schemaType,
+              style: TextStyle(
+                color: theme.colorScheme.onInverseSurface,
+                fontSize: 9,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
