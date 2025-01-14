ff7396c8b8ccf130aa7d1309f5386d5fda542dda
pavanpodila <pavan@vyuh.tech>
2025-01-14 17:13:50 +0530

    fix: fixed analysis issue

--- packages/system/vyuh_core/lib/plugin/network/network_plugin.dart
+++ packages/system/vyuh_core/lib/plugin/network/network_plugin.dart
@@ -4,14 +4,14 @@ import 'package:http/http.dart';
 import 'package:vyuh_core/vyuh_core.dart';
 
 /// Base class for implementing network communication in Vyuh applications.
-/// 
+///
 /// The network plugin provides HTTP client functionality with additional
 /// features like:
 /// - Automatic retry on failure
 /// - Request/response interceptors
 /// - Default headers and configurations
 /// - Error handling and logging
-/// 
+///
 /// This plugin implements the standard Dart [Client] interface, making it
 /// compatible with existing HTTP client code.
 abstract class NetworkPlugin extends Plugin
@@ -20,39 +20,39 @@ abstract class NetworkPlugin extends Plugin
   NetworkPlugin({required super.name, required super.title}) : super();
 
   /// Sends an HTTP GET request to the given URL.
-  /// 
+  ///
   /// The [headers] parameter can be used to include additional HTTP headers
   /// with the request.
   @override
   Future<Response> get(Uri url, {Map<String, String>? headers});
 
   /// Sends an HTTP HEAD request to the given URL.
-  /// 
+  ///
   /// The [headers] parameter can be used to include additional HTTP headers
   /// with the request.
   @override
   Future<Response> head(Uri url, {Map<String, String>? headers});
 
   /// Sends an HTTP POST request to the given URL.
-  /// 
+  ///
   /// - [headers]: Additional HTTP headers for the request
-  /// - [body]: The request body (can be String, List<int>, or Map)
+  /// - [body]: The request body (can be String, `List<int>`, or Map)
   /// - [encoding]: The encoding to use for the request body
   @override
   Future<Response> post(Uri url,
       {Map<String, String>? headers, Object? body, Encoding? encoding});
 
   /// Sends an HTTP PUT request to the given URL.
-  /// 
+  ///
   /// - [headers]: Additional HTTP headers for the request
-  /// - [body]: The request body (can be String, List<int>, or Map)
+  /// - [body]: The request body (can be String, `List<int>`, or Map)
   /// - [encoding]: The encoding to use for the request body
   @override
   Future<Response> put(Uri url,
       {Map<String, String>? headers, Object? body, Encoding? encoding});
 
   /// Sends an HTTP DELETE request to the given URL.
-  /// 
+  ///
   /// - [headers]: Additional HTTP headers for the request
   /// - [body]: Optional request body
   /// - [encoding]: The encoding to use for the request body
@@ -61,7 +61,7 @@ abstract class NetworkPlugin extends Plugin
       {Map<String, String>? headers, Object? body, Encoding? encoding});
 
   /// Sends an HTTP PATCH request to the given URL.
-  /// 
+  ///
   /// - [headers]: Additional HTTP headers for the request
   /// - [body]: The request body with changes to apply
   /// - [encoding]: The encoding to use for the request body
