c67a3cc9d76366b0208de82e1164827604b9322f
pavanpodila <pavan@vyuh.tech>
2025-01-20 19:37:25 +0530

    adding a deployed version of studio

--- apps/demo-studio/sanity.cli.ts
+++ apps/demo-studio/sanity.cli.ts
@@ -1,8 +1,9 @@
-import {defineCliConfig} from 'sanity/cli'
+import { defineCliConfig } from 'sanity/cli';
 
 export default defineCliConfig({
   api: {
     projectId: '8b76lu9s',
     dataset: 'production',
   },
-})
+  studioHost: 'vyuh',
+});
