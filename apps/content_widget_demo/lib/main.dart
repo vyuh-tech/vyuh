4bfe924be929109b5b674ef7834bf303895a79d5
pavanpodila <pavan@vyuh.tech>
2025-02-15 08:17:15 +0530

    fixing the example for better flutter hot reload

--- apps/content_widget_demo/lib/main.dart
+++ apps/content_widget_demo/lib/main.dart
@@ -14,6 +14,7 @@ import 'package:vyuh_plugin_telemetry_provider_console/vyuh_plugin_telemetry_pro
 
 import 'example.dart';
 
+// The examples to display in the dashboard.
 final examples = [
   const HelloWorldExample(),
   const CustomWidgetExample(),
@@ -21,19 +22,21 @@ final examples = [
   const RouteExample(),
 ];
 
-final sanityProvider = SanityContentProvider.withConfig(
-  config: SanityConfig(
-    projectId: '8b76lu9s',
-    dataset: 'production',
-    perspective: Perspective.previewDrafts,
-    useCdn: false,
-    token:
-        'skt2tSTitRob9TonNNubWg09bg0dACmwE0zHxSePlJisRuF1mWJOvgg3ZF68CAWrqtSIOzewbc56dGavACyznDTsjm30ws874WoSH3E5wPMFrqVW8C0Hc0pJGzpYQiehfL9GTRrIyoO3y2aBQIxHpegGspzxAevZcchleelaH5uM6LAnOJT1',
-  ),
-  cacheDuration: const Duration(seconds: 1),
-);
-
 void main() async {
+  // Initialize the Sanity content provider with the appropriate configuration.
+  final sanityProvider = SanityContentProvider.withConfig(
+    config: SanityConfig(
+      projectId: '8b76lu9s',
+      dataset: 'production',
+      perspective: Perspective.previewDrafts,
+      useCdn: false,
+      token:
+          'skt2tSTitRob9TonNNubWg09bg0dACmwE0zHxSePlJisRuF1mWJOvgg3ZF68CAWrqtSIOzewbc56dGavACyznDTsjm30ws874WoSH3E5wPMFrqVW8C0Hc0pJGzpYQiehfL9GTRrIyoO3y2aBQIxHpegGspzxAevZcchleelaH5uM6LAnOJT1',
+    ),
+    cacheDuration: const Duration(seconds: 1),
+  );
+
+  // Initialize VyuhContentBinding with the Sanity provider and the default content feature.
   VyuhContentBinding.init(
     plugins: PluginDescriptor(
       content: DefaultContentPlugin(provider: sanityProvider),
@@ -42,34 +45,45 @@ void main() async {
     descriptors: [system.descriptor],
   );
 
-  runApp(MyApp(examples: examples));
+  // Run the app.
+  runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  final List<ExampleWidget> examples;
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
 
-  const MyApp({super.key, required this.examples});
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // The GoRouter instance.
+  late final GoRouter _router = GoRouter(
+    routes: [
+      // The dashboard page route.
+      GoRoute(
+        path: '/',
+        builder: (context, state) => DashboardPage(examples: examples),
+      ),
+      // The route for each example.
+      GoRoute(
+        path: '/:index',
+        builder: (context, state) {
+          final index = int.parse(state.pathParameters['index']!);
+
+          // Return the content for the example.
+          return ExampleContent(example: examples[index]);
+        },
+      ),
+    ],
+  );
 
   @override
   Widget build(BuildContext context) {
+    // Return the MaterialApp with the GoRouter.
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
-      routerConfig: GoRouter(
-        routes: [
-          GoRoute(
-            path: '/',
-            builder: (context, state) => DashboardPage(examples: examples),
-          ),
-          GoRoute(
-            path: '/:index',
-            builder: (context, state) {
-              final index = int.parse(state.pathParameters['index']!);
-
-              return ExampleContent(example: examples[index]);
-            },
-          ),
-        ],
-      ),
+      routerConfig: _router,
     );
   }
 }
@@ -80,6 +94,7 @@ class DashboardPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Return the Scaffold with the list of examples.
     return Scaffold(
       appBar: AppBar(title: const Text('Content-driven UI')),
       body: SafeArea(
@@ -90,6 +105,7 @@ class DashboardPage extends StatelessWidget {
             itemBuilder: (_, index) {
               final ex = examples[index];
 
+              // Return a ListTile for each example.
               return ListTile(
                 title: Text(ex.title),
                 subtitle: Text(ex.description),
