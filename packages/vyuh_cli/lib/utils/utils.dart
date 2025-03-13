b175b96be03c43f508410a92ddcdf83aad8fff38
pavanpodila <pavan@vyuh.tech>
2025-03-13 15:54:00 +0530

    fix: adding dash in the regex for identifier

--- packages/vyuh_cli/lib/utils/utils.dart
+++ packages/vyuh_cli/lib/utils/utils.dart
@@ -7,7 +7,7 @@ typedef MasonGeneratorFromBundle = Future<MasonGenerator> Function(MasonBundle);
 
 typedef MasonGeneratorFromBrick = Future<MasonGenerator> Function(Brick);
 
-final RegExp identifierRegExp = RegExp('[a-z_][a-z0-9_]*');
+final RegExp identifierRegExp = RegExp('[a-z][-a-z0-9_-]*');
 final RegExp orgNameRegExp = RegExp(r'^[a-zA-Z][\w-]*(\.[a-zA-Z][\w-]*)+$');
 
 const String defaultCMS = 'sanity';
